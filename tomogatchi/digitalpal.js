9090fc948520ccf144f0bfe933b25b39cf7a45ca
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-08-07 02:04:39 -0700

    wrote 1st contructor
    created constructor to make objects for digitalpal

---
+++ tomogatchi/digitalpal.js
@@ -0,0 +1,8 @@
+//constructor used to create digital pal OBJECTS!
+const DigitalPal = function () {
+  this.hungry = false;
+  this.sleepy = false;
+  this.bored = true;
+  this.age = 0;
+  this.houseQuality = 100;
+};
