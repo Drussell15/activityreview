897d3db333949bf535e0f4f14d57a1c7250284c8
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-08-08 10:00:36 -0700

    final methods

--- tomogatchi/digitalpal.js
+++ tomogatchi/digitalpal.js
@@ -58,5 +58,65 @@ DigitalPal.prototype.destroyfurnature = function () {
     console.log(("Ive destroyed everything!!");
   }
 };
+// adds the "buyNewFurniture" method to the prototype which increases the "animals.cat.houseQuality" value by fifty
+DigitalPal.prototype.buyNewFurniture = function () {
+  this.houseQuality += 50;
+  console.log("Are you sure that's a good idea?");
+};
+
+// method which is called in "this.sleep" to increase the age of the digitalpal by one
+DigitalPal.prototype.increaseAge = function () {
+  this.age++;
+  console.log("Happy Birthday to me! I am " + this.age + " old!");
+};
+
+// adds the method "letoutside" to the prototype which lets "dog" outside when it is outside
+DigitalPal.prototype.letOutside = function () {
+  if (!this.outside) {
+    console.log("Yay! I love the outdoors!");
+    this.outside = true;
+    this.bark();
+  }
+  else {
+    console.log("We're already outside though...");
+  }
+};
+
+// adds the method "letInside" to the prototype which lets our DigitalPal inside when it is outside
+DigitalPal.prototype.letInside = function () {
+  if (this.outside) {
+    console.log("Aww... Do I have to?");
+    this.outside = false;
+  }
+  else {
+    console.log("We're already inside though...");
+  }
+};
+
+// adds the method "bark" to the prototype which lets our digitalpal bark
+DigitalPal.prototype.bark = function () {
+  console.log("Woof! Woof!");
+};
+
+// adds the "meow" method to the prototype which lets our pet meow.
+DigitalPal.prototype.meow = function () {
+  console.log("Meow! Meow!");
+};
+
+// Create a new animals object to contain our new Digital Pals
+const animals = {};
+
+// creates a new DigitalPal object with the name "dog"
+animals.dog = new DigitalPal();
+
+// creates a new DigitalPal object with the name "cat"
+animals.cat = new DigitalPal();
+
+// Grabbing command line arguments for animal and method
+const animal = process.argv[2];
+const method = process.argv[3];
+
+// try calling your chosen animal and method from the terminal example: 'node tamagotchi cat meow'
+animals[animal][method]();
 
 
