e8e6db7ea0ccf5bb817eb814fd8c4b7248c59d41
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-08-08 09:54:39 -0700

    added methods
    put to sleep if tired
    play if bored

--- tomogatchi/digitalpal.js
+++ tomogatchi/digitalpal.js
@@ -6,3 +6,43 @@ const DigitalPal = function () {
   this.age = 0;
   this.houseQuality = 100;
 };
+
+//methods
+DigitalPal.prototype.feed = function () {
+  if (this.hungry) {
+    console.log("That was yummy!");
+    this.hungry = false;
+    this.sleepy = true;
+  }
+  else {
+    console.log("No thanks, I'm full.");
+  }
+};
+
+//method to put asleep when sleepy,increase age by1 set to bored.
+
+DigitalPal.prototype.sleep = fuction() {
+  if * this.sleepy) {
+    console.log("ZZZZzzzZZZZzzzz");
+    this.sleepy = false;
+    this.bored = true;
+    this.increaseAge();
+  }
+  else {
+    console.log("No, I want to play!");
+  }
+};
+
+//method to let user play when bored, set to hungry
+
+DigitalPal.prototype.play = function () {
+  if (this, bored) {
+    console.log("yay, lets play");
+    this, bored = false;
+    this.hungry = true;
+  }
+  else {
+    console.log("not right now, maybe later?");
+  }
+};
+
